b03d2aa99b095f04121a250f3a33b244d0e95f94
komal Nagargoje <nagargojekomal341@gmail.com>
2025-09-24 17:33:57 +0530

    error solving

--- main.py
+++ main.py
@@ -23,40 +23,42 @@ class recipeRequest(BaseModel):
 
 @app.post("/generate")
 async def get_ingredients(data: recipeRequest):
-    ingredients = data.ingredients
+    ingredient = data.ingredients
 
     # Safe SELECT
-    recipes = query(f"SELECT * FROM recipes WHERE ingrediant={ingredients}")
-    if recipes:
-     steps_from_db = recipes[0][2].split("\n")
-     return JSONResponse(content={"message": "Data from DB", "recipe": steps_from_db})
-    try:
-        gemini_key = "AIzaSyAdfD6yDtOF6vbmoxVvtHcuG4SVPTMx_fg"
-        client = OpenAI(
-            api_key=gemini_key,
-            base_url="https://generativelanguage.googleapis.com/v1beta/openai/"
-        )
+    recipes = query("SELECT * FROM recipes WHERE ingredient = %s", (ingredient,))
+    logger.info(f"Query result: {recipes}")
+    if len(recipes) > 0: 
+        steps_from_db = recipes[0][2].split("\n")
+        return JSONResponse(content={"message": "Data from DB", "recipe": steps_from_db})
+    else:
+        try:
+            gemini_key = "AIzaSyAdfD6yDtOF6vbmoxVvtHcuG4SVPTMx_fg"
+            client = OpenAI(
+                api_key=gemini_key,
+                base_url="https://generativelanguage.googleapis.com/v1beta/openai/"
+            )
 
-        response = client.chat.completions.create(
-            model="gemini-2.5-flash",
-            messages=[
-                {"role": "system", "content": "You are an expert recipe generator."},
-                {"role": "user", "content": f"Give me recipes using these ingredients: {ingredients}"}
-            ]
-        )
+            response = client.chat.completions.create(
+                model="gemini-2.5-flash",
+                messages=[
+                    {"role": "system", "content": "You are an expert recipe generator."},
+                    {"role": "user", "content": f"Give me recipes using these ingredients: {ingredient}"}
+                ]
+            )
 
-        result_text = response.choices[0].message.content
-        steps = result_text.split("\n")
+            result_text = response.choices[0].message.content
+            steps = result_text.split("\n")
 
-        if not steps:
-            return {"message": "No ingredients found for this recipe"}
+            if not steps:
+                return {"message": "No ingredients found for this recipe"}
 
 
-        steps_text = "\n".join(steps)
-        query("INSERT INTO recipes (ingredient, steps) VALUES (%s, %s)", (ingredients, steps_text))
-       
-        return {"message": "Data fetched from Gemini API", "recipe": steps}
+            steps_text = "\n".join(steps)
+            query("INSERT INTO recipes (ingredient, steps) VALUES (%s, %s)", (ingredient, steps_text))
+        
+            return {"message": "Data fetched from Gemini API", "recipe": steps}
 
-    except Exception as e:
-        logger.error(str(e))
-        return {"message": "Error fetching data", "error": str(e)}
+        except Exception as e:
+            logger.error(str(e))
+            return {"message": "Error fetching data", "error": str(e)}
